29c528f8b90d1fcd430f3db39d2175c70f506cca
Davide Galassi <davxy@datawok.net>
2022-04-07 14:26:49 +0200

    sync script

---
+++ sync.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp $HOME/.bashrc home/bashrc
+cp $HOME/.gitconfig home/gitconfig
+cp $HOME/.tmux.conf home/tmux.conf
+cp $HOME/.vimrc home/vimrc
+cp $HOME/.Xdefaults home/Xdefaults
+
+cp /etc/samba/smb.conf etc/smb.conf
