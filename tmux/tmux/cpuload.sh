4ba2c272d50b877e98710cc8eb398a176cde7ec9
Davide Galassi <davxy@datawok.net>
2022-09-20 10:42:37 +0200

    Tmux: config update

--- tmux/tmux/cpuload.sh
+++ tmux/tmux/cpuload.sh
@@ -3,7 +3,7 @@
 case "$OSTYPE" in
     linux-gnu)
         CPU=`eval $(awk '/^cpu /{print "previdle=" $5 "; prevtotal=" $2+$3+$4+$5 }' /proc/stat); sleep 0.4; eval $(awk '/^cpu /{print "idle=" $5 "; total=" $2+$3+$4+$5 }' /proc/stat); intervaltotal=$((total-${prevtotal:-0})); echo "$((100*( (intervaltotal) - ($idle-${previdle:-0}) ) / (intervaltotal) ))"`
-        FREE_MEM=`free | grep "Mem" | awk '{print (100 - ($4/($3+$4) * 100.0));}'`
-        printf "C: %.f%% M: %.f%% " $CPU $FREE_MEM
+        FREE_MEM=`free | grep "Mem" | awk '{print $3/$2*100;}'`
+        printf "%3.d%%C %3.d%%M " $CPU $FREE_MEM
         ;;
 esac
