ae0167b3a8d737810295fec7b8e615be138b1ec9
Davide Galassi <davxy@datawok.net>
2022-10-20 10:03:36 +0200

    tmux: cpuload script fix

--- tmux/cpuload.sh
+++ tmux/cpuload.sh
@@ -3,4 +3,4 @@
 CPU=`eval $(awk '/^cpu /{print "previdle=" $5 "; prevtotal=" $2+$3+$4+$5 }' /proc/stat); sleep 0.4; eval $(awk '/^cpu /{print "idle=" $5 "; total=" $2+$3+$4+$5 }' /proc/stat); intervaltotal=$((total-${prevtotal:-0})); echo "$((100*( (intervaltotal) - ($idle-${previdle:-0}) ) / (intervaltotal) ))"`
 FREE_MEM=`free | grep "Mem" | awk '{print $3/$2*100;}'`
 
-printf "%2.d%%C %2.d%%M" $CPU $FREE_MEM
+printf "%2.0f%%C %2.0f%%M" $CPU $FREE_MEM
