eeaf09cde678766fa0af1d2c77ba9679f49968ad
Davide Galassi <davxy@datawok.net>
2022-11-29 10:01:44 +0100

    wob: configuration file

--- sway/wob.sh
+++ sway/wob.sh
@@ -2,13 +2,4 @@
 
 pkill tail
 rm -rf $SWAYSOCK.wob && mkfifo $SWAYSOCK.wob
-tail -f $SWAYSOCK.wob | wob --anchor "top" \
-    --anchor "right" \
-    --width  300 \
-    --height 20 \
-    --offset 0 \
-    --border 1 \
-    --margin 10 \
-    --background-color '#262626aa' \
-    --border-color '#d4be98aa' \
-    --bar-color '#d4be98aa'
+tail -f $SWAYSOCK.wob | wob
