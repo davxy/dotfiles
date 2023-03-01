adf5314f95ef7ed69252bbe7dc25b4575e4b2438
Davide Galassi <davxy@datawok.net>
2023-03-01 19:11:37 +0100

    ranger: encryption plugin

--- ranger/commands/crypt.py
+++ ranger/commands/crypt.py
@@ -1,31 +1,97 @@
-from ranger.api.commands import Command
-from ranger.core.loader import CommandLoader
 import os
+import tarfile
+from gnupg import GPG
+from ranger.api.commands import Command
+from subprocess import run
+import subprocess
+from getpass import getpass
+
+import ranger.api
+
+
+def get_passphrase(cmd):
+    command = 'python -c "from getpass import getpass; print(getpass())"'
+    psw = cmd.fm.execute_command(command, stdout=subprocess.PIPE)
+    stdout, _ = psw.communicate()
+    if psw.returncode == 0:
+        return stdout.strip().decode('utf-8')
+    else:
+        return None
+
 
 class encrypt(Command):
+    """:encrypt
+
+    Encrypts a file or directory using gpg
     """
-    :encrypt
-    Encrypts a folder or a file using gpg symmetric encryption.
-    Folders are first archived using tar.
-    """
-    
+
+    def tardir(self, path):
+        """
+        tars a directory into a dir of the same name appended with .tar.gz
+
+        returns the name of the tarfile
+        """
+        output_path = path + '.tar.gz'
+
+        with tarfile.open(output_path, "w:gz") as tar_handle:
+            for root, dirs, files in os.walk(path):
+                for file in files:
+                    tar_handle.add(os.path.join(root, file))
+
+        return output_path
+
+
     def execute(self):
-        cwd = self.fm.thisdir
-        marked_files = cwd.get_selection()
-        if not marked_files:
+        gpg = GPG()
+        recipient = None
+        passphrase = get_passphrase(self)
+        if passphrase is None:
+            self.fm.notify('Unable to read passphrase', bad=True)
             return
-        
-        def refresh(_):
-            cwd = self.fm.get_directory(original_path)
-            cwd.load_content()
 
-        original_path = cwd.path
-        parts = self.line.split()
-        
-        descr = "encrypting files in: " + os.path.basename(parts[1])
+        paths = [os.path.basename(f.path) for f in self.fm.thistab.get_selection()]
 
-        obj = CommandLoader(args=['gpg --encrypt'] + \
-            [os.path.relpath(f.path, cwd.path) for f in marked_files], descr=descr, read=True)        
-        
-        obj.signal_bind('after', refresh)
-        self.fm.loader.add(obj)
+        for p in paths:
+            if os.path.isdir(p):
+                new_p = self.tardir(p)
+                run(['rm', '-rf', p])
+                p = new_p
+
+            with open(p, 'rb') as f:
+                enc = gpg.encrypt_file(f, recipient, symmetric=True, passphrase=passphrase)
+
+            with open(p + '.gpg', 'wb+') as out:
+                out.write(enc.data)
+
+            if os.path.isfile(p):
+                os.remove(p)
+
+class decrypt(Command):
+    """:decrypts
+
+    Decrypts a file or a directory with gpg
+    """
+           
+    def execute(self):
+        gpg = GPG()
+        passphrase = get_passphrase(self)
+        if passphrase is None:
+            self.fm.notify('Unable to read passphrase', bad=True)
+            return
+
+        paths = [os.path.basename(f.path) for f in self.fm.thistab.get_selection()]
+
+        for p in [p for p in paths if p.endswith('gpg')]:
+            with open(p, 'rb') as enc:
+                dec_res = gpg.decrypt_file(enc, passphrase=passphrase)
+                if dec_res.ok is False:
+                    self.fm.notify('Decryption failure', bad=True)
+                    return
+
+            filename = os.path.splitext(p)[0]
+            with open(filename, 'wb+') as file:
+                file.write(dec_res.data)
+            os.remove(p)
+            if tarfile.is_tarfile(filename):
+                tarfile.open(filename).extractall(path='.')
+                os.remove(filename)
