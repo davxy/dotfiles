2d19d81707de3cbdfb4277a6004e7a2f5493c039
Davide Galassi <davxy@datawok.net>
2024-12-31 09:43:08 +0100

    yazi: update CWD_FILE

--- common/yazi/plugins/folder-rules.yazi/init.lua
+++ common/yazi/plugins/folder-rules.yazi/init.lua
@@ -1,6 +1,18 @@
+local function write_cwd_to_file(cwd)
+		local output_file = os.getenv("CWD_FILE")
+    local file = io.open(output_file, "w")
+    if file then
+        file:write(cwd .. "\n")
+        file:close()
+    else
+        print("Error: Unable to write to file: " .. output_file)
+    end
+end
+
 local function setup()
 	ps.sub("cd", function()
 		local cwd = cx.active.current.cwd
+		write_cwd_to_file(cwd)
 		if cwd:ends_with("Downloads") then
 			ya.manager_emit("sort", { "modified", reverse = true, dir_first = false })
 		else
