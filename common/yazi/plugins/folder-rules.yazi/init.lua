16a5e253f9555d919faba69aecef63bcf52b7ebb
Davide Galassi <davxy@datawok.net>
2025-01-01 10:45:35 +0100

    yazi: update zoxide on cd

--- common/yazi/plugins/folder-rules.yazi/init.lua
+++ common/yazi/plugins/folder-rules.yazi/init.lua
@@ -9,10 +9,17 @@ local function write_cwd_to_file(cwd)
     end
 end
 
+-- Run the zoxide add command for the entered folder
+local function update_zoxide(cwd)
+    local command = "zoxide add " .. tostring(cwd)
+    os.execute(command)
+end
+
 local function setup()
 	ps.sub("cd", function()
 		local cwd = cx.active.current.cwd
 		write_cwd_to_file(cwd)
+		update_zoxide(cwd)
 		if cwd:ends_with("Downloads") then
 			ya.manager_emit("sort", { "modified", reverse = true, dir_first = false })
 		else
